822f703971bb2ac26e7cc4b5c1698ff426573201
ReluxZx <jnjsmayo@gmail.com>
2023-10-05 20:31:44 -0500

    crud

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function app(){
 
     let crud = new CRUD("FOR EXAMPLE ");
     crud.create([1 , 2 , 3]);
-    crud.create({ message: "buenas que pasa chavales y tal "});
+    crud.create({ message: "buenas que pasa chavales y tal"});
 
     let crud2 = new CRUD ("FOR EXAMPLE ");
     console.log(crud2.readALL());
